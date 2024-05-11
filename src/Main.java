2b36ac6e9c75eae7eb9cfa90d0c13208a817d5e1
Lucas Rodrigues <lucas09rm@gmail.com>
2024-05-11 00:10:29 -0300

    Criando projeto Petland

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import com.petland.domain.cadastro.Cadastro;
+import com.petland.domain.cadastro.Endereco;
+import com.petland.domain.cadastro.Perfil;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Cadastro cadastro = new Cadastro();
+        cadastro.setId(1);
+        cadastro.setNome("Lucas Rodrigues");
+
+        Perfil perfil = new Perfil();
+        perfil.setCliente(true);
+        cadastro.setPerfil(perfil);
+
+        Endereco endereco = new Endereco();
+        endereco.setLogradouro("Rua plinio pereira");
+        endereco.setNumero("77");
+        cadastro.setEndereco(endereco);
+
+        System.out.println("Cadastro: "+cadastro);
     }
 }
---
+++ src/com/petland/domain/cadastro/Endereco.java
@@ -0,0 +1,23 @@
+package com.petland.domain.cadastro;
+
+public class Endereco {
+
+    private String logradouro;
+    private String numero;
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+}
